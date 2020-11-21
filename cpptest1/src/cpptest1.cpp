c6b4cc19bb416239d169dff59f115473a0c243f7
Oleg Galbert <ogcraft@gmail.com>
2020-11-21 11:20:01 +0200

    Make parse arguments more suitable for unitesting

--- cpptest1/src/cpptest1.cpp
+++ cpptest1/src/cpptest1.cpp
@@ -10,7 +10,12 @@
 
 using namespace std;
 
-void parse(int argc, const char* argv[])
+struct AppConfig {
+  bool help = false;
+  bool verbose = false;
+} appCfg;
+
+int parse(int argc, const char* argv[], AppConfig& cfg)
 {
   try
   {
@@ -19,14 +24,11 @@ void parse(int argc, const char* argv[])
       .positional_help("[optional args]")
       .show_positional_help();
 
-    bool help = false;
-    bool verbose = false;
-
     options
       .allow_unrecognised_options()
       .add_options()
-      ("h,help", "Print help", cxxopts::value<bool>(help))
-      ("v,verbose", "Print debug output", cxxopts::value<bool>(verbose))
+      ("h,help", "Print help", cxxopts::value<bool>(cfg.help))
+      ("v,verbose", "Print debug output", cxxopts::value<bool>(cfg.verbose))
     ;
 
     auto result = options.parse(argc, argv);
@@ -34,7 +36,7 @@ void parse(int argc, const char* argv[])
     if (result.count("help"))
     {
       std::cout << options.help({"", "Group"}) << std::endl;
-      exit(0);
+      return EXIT_SUCCESS;
     }
 
     std::cout << "Arguments remain = " << argc << std::endl;
@@ -45,14 +47,20 @@ void parse(int argc, const char* argv[])
   catch (const cxxopts::OptionException& e)
   {
     std::cout << "error parsing options: " << e.what() << std::endl;
-    exit(1);
+    return EXIT_FAILURE;
   }
+  return EXIT_SUCCESS;
 }
 
 int main(int argc, const char* argv[]) {
+    
+    int ret = EXIT_SUCCESS;
     cout << "Starting main() on " << std::thread::hardware_concurrency() << " cores" << "\n";
 
-    parse(argc, argv);
-
-    return EXIT_SUCCESS;
+    if ( (ret = parse(argc, argv, appCfg)) != EXIT_SUCCESS) {
+      cout << "Argument parsing failed. Exit main() with " << ret << "\n";
+      exit(ret);
+    }
+    if(appCfg.verbose) cout << "Exit main() with " << ret << "\n";
+    exit(ret);
 }
